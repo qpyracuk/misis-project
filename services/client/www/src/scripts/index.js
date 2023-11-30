708a3e7e8a703288871e95e1b7b0f21f31caecb0
Pobedisnkiy David <qpyracuk@gmail.com>
2023-11-30 22:41:38 +0300

    инициализация микросервисов. поднял nginx для статики и обратного проксирования, redis для кэша, postgres для хранения. контейнер сервера работает на node.js + express

--- services/client/www/src/scripts/index.js
+++ services/client/www/src/scripts/index.js
@@ -1,29 +1,43 @@
 (function () {
-  const logo = document.getElementById("logo");
+	const logo = document.getElementById("logo");
 	const mouseHandler = function (e) {
-		logo.style.setProperty("--logo-rotate", `${ (e.clientX + e.clientY) / 10 }deg`);
+		logo.style.setProperty("--logo-rotate", `${(e.clientX + e.clientY) / 10}deg`);
 	};
 	document.addEventListener("mousemove", mouseHandler.bind(this));
 })();
 
 const input = document.getElementById("clicker-input");
 const button = document.getElementById("clicker-button");
+const qrCanvas = document.getElementById("qr-code");
+const shortcutHref = document.getElementById("short-href");
 
 const generateQrCode = (url) => {
-  const qr = new QRCode(document.getElementById("qr-code"), {
-    text: url,
-    width: 220,
-    height: 220,
-  });
+	qrCanvas.innerHTML = "";
+	const qr = new QRCode(qrCanvas, {
+		text: url,
+		width: 220,
+		height: 220,
+	});
 };
 
-const URL = `${window.location.hostname}:10080/reduce`
-const shortUrl = document.getElementById("short-url")
-button.addEventListener("click", (e) => {
-  fetch(URL, JSON.stringify({ url: input.target.value })).then(response => {
-    shortUrl.target.value = response.short
-    generateQrCode(response.short)
-  }).catch(error => {
-    console.log("Сервер недоступен");
-  })
-})
+const URL = `http://${window.location.hostname}:80/reduce`;
+const shortUrl = document.getElementById("short-url");
+button.addEventListener("click", async () => {
+	fetch(URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ url: input.value }),
+	})
+		.then((response) => response.json())
+    .then((json) => {
+      if (json !== undefined && json.type === "correct") {
+        console.log("URL ",json.url);
+				shortUrl.innerText = json.url;
+				shortcutHref.setAttribute("href", json.url);
+				generateQrCode(json.url);
+			}
+		})
+		.catch((error) => {});
+});
